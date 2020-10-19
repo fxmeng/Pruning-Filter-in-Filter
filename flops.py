36d504a3bfed32e9db93eaae8ed31aed7327629d
fxmeng <60565778+fxmeng@users.noreply.github.com>
2020-10-19 12:31:40 +0800

    Add files via upload

---
+++ flops.py
@@ -0,0 +1,92 @@
+# Code from https://github.com/simochen/model-tools.
+import numpy as np
+import os
+from models.stripe import *
+import torch
+import torchvision
+import torch.nn as nn
+
+
+def print_model_param_nums(model=None):
+    total = sum([param.nelement() if param.requires_grad else 0 for param in model.parameters()])
+    print('  + Number of params: %.2fM' % (total / 1e6))
+
+
+def count_model_param_flops(model=None, input_res=32, multiply_adds=True):
+    list_strip = []
+
+    def filter_strip_hook(self, input, output):
+        output_channels, output_height, output_width = output[0].size()
+        flops = self.kernel_size[0] * self.kernel_size[1] * self.in_channels * (2 if multiply_adds else 1) * self.out_channels * output_height * output_width * input[0].size()[0]
+        list_strip.append(flops)
+
+    list_linear = []
+
+    def linear_hook(self, input, output):
+        batch_size = input[0].size(0) if input[0].dim() == 2 else 1
+
+        weight_ops = self.weight.nelement() * (2 if multiply_adds else 1)
+        bias_ops = self.bias.nelement()
+
+        flops = batch_size * (weight_ops + bias_ops)
+        list_linear.append(flops)
+
+    list_bn = []
+
+    def bn_hook(self, input, output):
+        list_bn.append(input[0].nelement() * 2)
+
+    list_relu = []
+
+    def relu_hook(self, input, output):
+        list_relu.append(input[0].nelement())
+
+    list_pooling = []
+
+    def pooling_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+
+        kernel_ops = self.kernel_size * self.kernel_size
+        bias_ops = 0
+        params = 0
+        flops = (kernel_ops + bias_ops) * output_channels * output_height * output_width * batch_size
+
+        list_pooling.append(flops)
+
+    list_upsample = []
+
+    # For bilinear upsample
+    def upsample_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+
+        flops = output_height * output_width * output_channels * batch_size * 12
+        list_upsample.append(flops)
+
+    def foo(net):
+        childrens = list(net.children())
+        if not childrens:
+            if isinstance(net, FilterStripe):
+                net.register_forward_hook(filter_strip_hook)
+            if isinstance(net, Linear):
+                net.register_forward_hook(linear_hook)
+            if isinstance(net, BatchNorm):
+                net.register_forward_hook(bn_hook)
+            if isinstance(net, torch.nn.ReLU):
+                net.register_forward_hook(relu_hook)
+            if isinstance(net, torch.nn.MaxPool2d) or isinstance(net, torch.nn.AvgPool2d):
+                net.register_forward_hook(pooling_hook)
+            if isinstance(net, torch.nn.Upsample):
+                net.register_forward_hook(upsample_hook)
+            return
+        for c in childrens:
+            foo(c)
+
+    foo(model)
+    input = torch.rand(1, 3, input_res, input_res).cuda()
+    out = model(input)
+
+    total_flops = (sum(list_strip) + sum(list_linear) + sum(list_bn) + sum(list_relu) + sum(list_pooling) + sum(list_upsample))
+
+    print('  + Number of FLOPs: %.2fM' % (total_flops / 1e6))
---
+++ main.py
@@ -0,0 +1,130 @@
+from __future__ import print_function
+import os
+import argparse
+import shutil
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import models
+from flops import *
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR training')
+parser.add_argument('--data_path', type=str, default='../data')
+parser.add_argument('--num_classes', type=int, default=10)
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--epochs', type=int, default=160, metavar='N',
+                    help='number of epochs to train (default: 160)')
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate (default: 0.1)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum (default: 0.9)')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--save', default='./checkpoint', type=str, metavar='PATH',
+                    help='path to save prune model (default: current directory)')
+parser.add_argument('--arch', default='ResNet56', type=str,
+                    help='architecture to use')
+parser.add_argument('--sr', type=float)
+parser.add_argument('--threshold', type=float)
+args = parser.parse_args()
+print(args)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+if args.num_classes == 10:
+    train_set = datasets.CIFAR10(args.data_path, train=True)
+    test_set = datasets.CIFAR10(args.data_path, train=False)
+else:
+    train_set = datasets.CIFAR100(args.data_path, train=True)
+    test_set = datasets.CIFAR100(args.data_path, train=False)
+train_set.transform = transforms.Compose([
+    transforms.Pad(4),
+    transforms.RandomCrop(32),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+test_set.transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=4)
+model = models.__dict__[args.arch](num_classes=args.num_classes)
+model.cuda()
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+history_score = np.zeros((args.epochs + 1, 3))
+
+
+def train(epoch):
+    model.train()
+    global history_score
+    avg_loss = 0.
+    train_acc = 0.
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.cuda(), target.cuda()
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        avg_loss += loss.item()
+        pred = output.data.max(1, keepdim=True)[1]
+        train_acc += pred.eq(target.data.view_as(pred)).cpu().sum()
+        loss.backward()
+        ###########learning the shape of filter with filter skeleton################
+        if args.sr and args.threshold:
+            model.update_skeleton(args.sr, args.threshold)
+        ############################################################################
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{}]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset), loss.item()))
+    history_score[epoch][0] = avg_loss / len(train_loader)
+    history_score[epoch][1] = train_acc / float(len(train_loader))
+
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = data.cuda(), target.cuda()
+        output = model(data)
+        test_loss += F.cross_entropy(output, target, size_average=False).item()
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+    return correct / float(len(test_loader.dataset))
+
+
+best_prec1 = 0.
+for epoch in range(args.epochs):
+    if epoch in [args.epochs * 0.5, args.epochs * 0.75]:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] *= 0.1
+    train(epoch)
+    prec1 = test()
+    history_score[epoch][2] = prec1
+    np.savetxt(os.path.join(args.save, 'train_record.txt'), history_score, fmt='%10.5f', delimiter=',')
+    if prec1 > best_prec1:
+        best_prec1 = prec1
+        torch.save(model.state_dict(), os.path.join(args.save, 'best.pth.tar'))
+print("Best accuracy: " + str(best_prec1))
+history_score[-1][0] = best_prec1
+np.savetxt(os.path.join(args.save, 'train_record.txt'), history_score, fmt='%10.5f', delimiter=',')
+##############pruning filter in filter without finetuning#################
+if args.sr and args.threshold:
+    model.load_state_dict(torch.load(os.path.join(args.save, 'best.pth.tar')))
+    print(model.prune(args.threshold))
+    test()
+    print(model)
+    torch.save(model.state_dict(), os.path.join(args.save, 'pruned.pth.tar'))
+    print_model_param_nums(model)
+    count_model_param_flops(model)
+#########################################################
